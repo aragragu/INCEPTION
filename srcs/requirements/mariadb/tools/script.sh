b4f40bc823a56a222eefc23954b78161f2cef429
aragragu <anasragragui76@gmail.com>
2025-08-11 13:55:31 +0100

    working ion ngnix container

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,9 +8,9 @@ service mariadb start
 sleep 5
 
 #printing some stats
-echo "mariadb DATABASE: \`${MYSQL_DB}\`"
-echo "mariadb USER_NAME: \`${MYSQL_USER}\`"
-echo "Starting to create \`${MYSQL_DB}\` database ..."
+echo "mariadb DATABASE: ${MYSQL_DB}"
+echo "mariadb USER_NAME: ${MYSQL_USER}"
+echo "Starting to create ${MYSQL_DB} database ..."
 
 #"mariadb" is the CLI of the client to interact with the mariadbd. and in this case it lets us execute SQL statement
 #directly, the -e option tells the client to run the SQL command directly from the CLI.
@@ -18,18 +18,19 @@ echo "Starting to create \`${MYSQL_DB}\` database ..."
 # (CREATE DATABASE) -> creates a database | (IF NOT EXISTS) -> prevents an error if the data base exists
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB"
 
-# (CREATE USER) -> creates a user account in mariadb | (IF NOT EXISTS) -> prevent 
+# (CREATE USER) -> creates a user account in mariadb | (IF NOT EXISTS) -> prevent an error if the user already exists
+# in
 mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%'"
 mariadb -e "FLUSH PRIVILEGES"
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysqladmin -u root shutdown
 
 echo "Finished creating \`${MYSQL_DB}\`..."
-
-# Keep MariaDB running in foreground
 echo "Starting MariaDB in foreground..."
 
-sleep 0.5
+# Keep MariaDB running in foreground
+
+# sleep 0.5
 
 exec mariadbd --bind-address=0.0.0.0
